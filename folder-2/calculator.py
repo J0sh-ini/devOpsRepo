dc91fd2280d31481da83771110e4466151b3d078
joshua <joshiniyan006@gmail.com>
2026-07-15 14:20:03 +0530

    calculator creation

---
+++ folder-2/calculator.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter n1: "))
+n2=int(input("enter n2: "))
+op=input("Enter operation to perform(add,sub):")
+if(op=="add"):
+    print(n1+n2)
+elif(op=="sub"):
+    print(n1-n2)
+else:
+    print("Invalid operation")
