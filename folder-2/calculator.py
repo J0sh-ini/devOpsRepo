425714d47f7e90928c7d1965c28d2b983f6993fa
joshua <joshiniyan006@gmail.com>
2026-07-15 14:25:49 +0530

    division feature added

--- folder-2/calculator.py
+++ folder-2/calculator.py
@@ -1,9 +1,14 @@
 n1=int(input("Enter n1: "))
 n2=int(input("enter n2: "))
-op=input("Enter operation to perform(add,sub):")
+op=input("Enter operation to perform(add,sub,div):")
 if(op=="add"):
     print(n1+n2)
 elif(op=="sub"):
     print(n1-n2)
+elif(op=="div"):
+    if(n2==0):
+        print("cant divide by 0")
+    else:
+        print(n1/n2)
 else:
     print("Invalid operation")
